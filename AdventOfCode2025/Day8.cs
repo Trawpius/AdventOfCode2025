1a0f62e0e361134dce4caf1aa36f226b9234dc18
LANSHE,MICHELLE (A-United States,ex1) <michelle.lanshe@agilent.com>
2025-12-09 14:20:25 -0500

    Add Day8 and Day9 but did not solve yet

---
+++ AdventOfCode2025/Day8.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2025
+{
+    public static class Day8
+    {
+        public static int Puzzle1()
+        {
+            int count = 0;
+            string filename = Path.Combine( Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "puzzles", "Day8Puzzle1.txt");
+            return count;
+        }
+
+       
+
+        public static long Puzzle2()
+        {
+            long count = 0;
+            string filename = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "puzzles", "Day8Puzzle1.txt");
+
+            return count;
+        }
+
+    }
+}
+
---
+++ AdventOfCode2025/Day9.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+namespace AdventOfCode2025
+{
+    public static class Day9
+    {
+        public static int Puzzle1()
+        {
+            int count = 0;
+            string filename = Path.Combine( Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "puzzles", "Day9Puzzle1.txt");
+
+            return count;
+        }
+
+
+        public static long Puzzle2()
+        {
+            long count = 0;
+            string filename = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "puzzles", "Day9Puzzle1.txt");
+            //string filename = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "puzzles", "test.txt");
+            
+            return count;
+        }
+
+    }
+}
+
